e4d19fb5d94cbd5d248a1870b6de302f7315d749
Kiydo <e.magtoto@hotmail.com>
2025-08-23 15:29:29 -0600

    slot controller for failed roll

--- gamejam45/Scripts/Objects/red_button.gd
+++ gamejam45/Scripts/Objects/red_button.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 @export var character_body_2d : CharacterBody2D = null
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var slots = get_parent().get_node("Slots")
 
 enum State {NOT_PRESSED, PRESSED}
+var current_state: State
+var result
 var animation_trigger : bool = false
 var fail_trigger : bool = false
 
@@ -24,3 +26,6 @@ func _on_hurtbox_body_entered(body: Node2D) -> void:
 	print("pressed red button")
 	animated_sprite_2d.play("pressed")
 	fail_trigger = true
+	print("Roll Failed")
+	result = false
+	slots.recive_roll(result)
